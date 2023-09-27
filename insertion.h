63de03e8f0f1f0b75c052026996fe4ef7c946891
Corey Lawrence <121775280+CoreyMLawrence@users.noreply.github.com>
2023-09-27 13:37:47 -0400

    added counter

--- insertion.h
+++ insertion.h
@@ -1,4 +1,4 @@
-void insertionSort (int n, int s[])
+void insertionSort (int n, int s[], int &counter)
 {
     int i, j, x;
     for(int i = 2; i <= n; i++)
@@ -9,6 +9,7 @@ void insertionSort (int n, int s[])
         {
             s[j+1] = s[j];
             j--;
+            counter++;
         }
         s[j + 1] = x;
     }
