bae2956717e233074597ae7f24bac9030476a203
toharaIII <146030293+toharaIII@users.noreply.github.com>
2023-09-28 13:53:49 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include "timeCounter.h"
+#include "insertion.h"
+#include "heapsort.h"
+#include "quicksort.h"
 #include <iostream>
 #include <ctime>
 using namespace std;
@@ -6,15 +10,23 @@ void clonearray(int master[], int clone[], int size);
 
 int main(int argc, const char * argv[]) {
     
-    int size=10;
+    int size=10; //need to change for submission
     int master [size]; //dont touch once loaded
     int clone [size]; //pass this 1 to sort functions
     
     srand(static_cast<unsigned int>(time(nullptr))); //creating rng
     for(int i=0; i<size; i++) master[i]=rand(); //adding values to master array
     
+    //heapsort calls, not working, know how to fix
+    heap H(size);
+    int endheap;
     clonearray(master,clone,size);
-    //call sort function
+    timeStart();
+    heapsort(size, H);
+    end=timeEnd();
+    cout<<"heapsort time taken: "<<end<<" microseconds"<<endl;
+    cout<<"heapsort had: "<<H.cnt<<" compares"<<endl;
+    
     
     return 0;
 }
