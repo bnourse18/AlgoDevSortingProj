1590a6fe2b9ae90488e40364eb08e8b7e182eb84
bnourse18 <bnourse@highpoint.edu>
2023-10-02 16:27:06 -0400

    Fix main, add quicksort counter

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main(int argc, const char * argv[]) {
     int size=10; //need to change for submission
     int master [size]; //dont touch once loaded
     int clone [size]; //pass this 1 to sort functions
+	long long end;
     
     srand(static_cast<unsigned int>(time(nullptr))); //creating rng
     for(int i=0; i<size; i++) master[i]=rand(); //adding values to master array
@@ -24,11 +25,19 @@ int main(int argc, const char * argv[]) {
     clonearray(master,clone,size);
     for(int i=0; i<size; i++) H.S[i]=clone[i];
     timeStart();
-    heapsort(size, H);
-    long long end=timeEnd();
-    cout<<"heapsort time taken: "<<end<<" microseconds"<<endl;
-    cout<<"heapsort had: "<<H.cnt<<" compares"<<endl;
-    
+    heapsort(size, H); //Run the heapsort 
+    end = timeEnd();
+    cout << "Heapsort time taken: " << end << " microseconds" << endl;
+    cout << "Heapsort had: " << H.cnt << " compares" << endl << endl;
+	
+	//quicksort calls, works
+	int quickcounter = 0;
+    clonearray(master,clone,size);
+	timeStart();
+	quicksort(0, size-1, clone, quickcounter); //Run the quicksort
+	end = timeEnd();
+	cout << "Quicksort runtime: " << end << " microseconds" << endl;
+	cout << "Quicksort performed " << quickcounter << " operations." << endl << endl;
     
     return 0;
 }
